a7a502872e3b4048c1cc6f270df8635d75d2fe37
Andi <28365760+And1B@users.noreply.github.com>
2022-01-02 14:45:36 +0100

    Update managing_orders.py

--- managing_orders.py
+++ managing_orders.py
@@ -2,7 +2,11 @@ tables = {
   1: {
     'name': 'Jiho',
     'vip_status': False,
-    'order': 'Orange Juice, Apple Juice'
+    'order': {
+      'drinks': ['Orange Juice', 'Apple Juice'],
+      'food_items': ['Pancakes'],
+      'total': [534.50, 20.0, 5]
+    }
   },
   2: {},
   3: {},
@@ -12,17 +16,38 @@ tables = {
   7: {},
 }
 print(tables)
-
 def assign_table(table_number, name, vip_status=False): 
   tables[table_number]['name'] = name
   tables[table_number]['vip_status'] = vip_status
-  tables[table_number]['order'] = ''
+  tables[table_number]['order'] = {}
 
-# Write your code below: 
-def assign_and_print_order(table_number, *order_items):
-  tables[table_number]['order'] = order_items
-  for order in order_items:
-    print(order)
+def assign_food_items(table_number, **order_items):
+  food = order_items.get('food')
+  drinks = order_items.get('drinks')
+  tables[table_number]['order']['food_items'] = food
+  tables[table_number]['order']['drinks'] = drinks
+
+def calculate_price_per_person(total, tip, split):
+    total_tip = total * (tip/100)
+    split_price = (total + total_tip) / split
+    print(split_price)
+    print('Total Amount:', total)
+#calculate_price_per_person(534,20.0,5)
+
+def remove_table(table_number):
+  tables[table_number] = {}
+
+def remove_items(table_number, item):
+  drink_list = tables[table_number]['order']['drinks']
+  food_list = tables[table_number]['order']['food_items']
+  if item in drink_list:
+    drink_list.remove(item)
+  elif item in food_list:
+    food_list.remove(item) 
+  else:
+    print("This order has not been added yet.")
+
+
+
 assign_table(2, 'Arwa', True)
-assign_and_print_order(2, 'Steak', 'Seabass', 'Wine Bottle')
 print(tables)
